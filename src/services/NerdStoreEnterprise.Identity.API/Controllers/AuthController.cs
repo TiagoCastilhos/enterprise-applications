d65ed6ca72e2d169f702958e4872055db08e14e3
Tiago Henrique de Castilhos <tiago.castilhos@promob.com>
2021-01-07 17:06:46 -0300

    sign in and authenticate basic implementation

---
+++ src/services/NerdStoreEnterprise.Identity.API/Controllers/AuthController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NerdStoreEnterprise.Identity.API.Models;
+using System.Threading.Tasks;
+
+namespace NerdStoreEnterprise.Identity.API.Controllers
+{
+    [Route("api/Identity")]
+    public class AuthController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> Register(RegisterUserViewModel registerUserViewModel)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var user = new IdentityUser
+            {
+                UserName = registerUserViewModel.Email,
+                Email = registerUserViewModel.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, registerUserViewModel.Senha);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login(UserLoginViewModel userLoginViewModel)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var result = await _signInManager.PasswordSignInAsync(userLoginViewModel.Email, userLoginViewModel.Senha, false, true);
+
+            if (result.Succeeded)
+                return Ok();
+
+            return BadRequest();
+        }
+    }
+}
