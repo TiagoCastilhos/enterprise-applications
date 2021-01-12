d688549c02d831cf6ce696c3ef076979d6a11705
Tiago castilhos <tiagoxd13@hotmail.com>
2021-01-12 20:57:46 -0300

    daily commit

---
+++ src/web/NerdStoreEnterprise.WebApp.MVC/Configuration/AuthConfig.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NerdStoreEnterprise.WebApp.MVC.Configuration
+{
+    public static class AuthConfig
+    {
+        public static void AddAuth(this IServiceCollection services)
+        {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/login";
+                    options.AccessDeniedPath = "/forbidden";
+                });
+        }
+
+        public static void UseAuth(this IApplicationBuilder app)
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+        }
+    }
+}
---
+++ src/web/NerdStoreEnterprise.WebApp.MVC/Controllers/IdentityController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using NerdStoreEnterprise.WebApp.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace NerdStoreEnterprise.WebApp.MVC.Controllers
+{
+    public class IdentityController : MainController
+    {
+        [HttpGet]
+        [Route("register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> RegisterAsync(RegisterUserViewModel registerUserViewModel)
+        {
+            if (!ModelState.IsValid) return View(registerUserViewModel);
+
+            var resposta = await _autenticacaoService.Register(registerUserViewModel);
+
+            if (ResponsePossuiErros(resposta.ResponseResult)) return View(registerUserViewModel);
+
+            await PerformLoginAsync(resposta);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+
+            return View();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> LoginAsync(UserLoginViewModel userLoginViewModel, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+
+            if (!ModelState.IsValid) return View(usuarioLogin);
+
+            var resposta = await _autenticacaoService.Login(usuarioLogin);
+
+            if (ResponsePossuiErros(resposta.ResponseResult)) return View(usuarioLogin);
+
+            await PerformLoginAsync(resposta);
+
+            if (string.IsNullOrEmpty(returnUrl)) return RedirectToAction("Index", "Home");
+
+            return LocalRedirect(returnUrl);
+        }
+
+        [HttpGet]
+        [Route("logout")]
+        public async Task<IActionResult> LogoutAsync(string returnUrl = null)
+        {
+            return View();
+        }
+
+        private async Task RealizarLogin(UsuarioRespostaLogin resposta)
+        {
+            var token = ObterTokenFormatado(resposta.AccessToken);
+
+            var claims = new List<Claim>();
+            claims.Add(new Claim("JWT", resposta.AccessToken));
+            claims.AddRange(token.Claims);
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var authProperties = new AuthenticationProperties
+            {
+                ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(60),
+                IsPersistent = true
+            };
+
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity),
+                authProperties);
+        }
+
+        private static JwtSecurityToken ObterTokenFormatado(string jwtToken)
+        {
+            return new JwtSecurityTokenHandler().ReadToken(jwtToken) as JwtSecurityToken;
+        }
+    }
+}
