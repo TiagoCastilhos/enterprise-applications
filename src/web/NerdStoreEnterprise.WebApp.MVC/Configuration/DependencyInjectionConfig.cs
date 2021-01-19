ac430f6d413aee73753ab8dd6659dd698d0b8981
Tiago Henrique de Castilhos <tiago.castilhos@promob.com>
2021-01-19 13:29:26 -0300

    DelegatingHandler

--- src/web/NerdStoreEnterprise.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/NerdStoreEnterprise.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using NerdStoreEnterprise.WebApp.MVC.Extensions;
 using NerdStoreEnterprise.WebApp.MVC.Services.Catalog;
+using NerdStoreEnterprise.WebApp.MVC.Services.Handlers;
 using NerdStoreEnterprise.WebApp.MVC.Services.Identity;
 
 namespace NerdStoreEnterprise.WebApp.MVC.Configuration
@@ -10,8 +11,13 @@ namespace NerdStoreEnterprise.WebApp.MVC.Configuration
     {
         public static IServiceCollection RegisterServices(this IServiceCollection services)
         {
+            services.AddTransient<HttpClientAuthorizationDelegatingHandler>();
+
             services.AddHttpClient<IAuthService, AuthenticationService>();
-            services.AddHttpClient<ICatalogService, CatalogService>();
+
+            services.AddHttpClient<ICatalogService, CatalogService>()
+                .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
+
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IUser, AspNetUser>();
 
